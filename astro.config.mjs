08edf8c9baafd7393c18bdce30ba1b7fcdb049eb
Kevin-Moreau65 <kevin.moreau655@gmail.com>
2023-05-24 16:52:21 +0200

    Revert "Added vercel adapter & analytic"
    This reverts commit df18a1a3a8d6117a3190e2ed8f27796e6b32fff1.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercelStatic from '@astrojs/vercel/static';
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
@@ -8,13 +10,9 @@ import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig( {
   site: 'https://kevin-moreau.vercel.app',
-  output: 'static',
-  adapter: vercelStatic(),
-  integrations: [ sitemap(), image( {
+  integrations: [ svelte(), sitemap(), image( {
     serviceEntryPoint: '@astrojs/image/sharp'
   } ) ]
 } );
